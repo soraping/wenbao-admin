ad2c40cb4337f47e6aeb258a18045ed8587eaf95
of1723 <caoshiping@qianmi.com>
2023-02-27 18:02:49 +0800

    wenbao

---
+++ src/config/website.config.ts
@@ -0,0 +1,9 @@
+import logoImage from '@/assets/images/logo.png';
+import loginImage from '@/assets/images/account-logo.png';
+
+export const websiteConfig = Object.freeze({
+  title: 'wenbaoAdmin',
+  logo: logoImage,
+  loginImage: loginImage,
+  loginDesc: 'Wenbao Admin 管理端',
+});
